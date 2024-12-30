418c3da0106c36faef1ce6897f5bb8fd3f917e28
Caleb Wishart <caleb.wish@outlook.com>
2024-12-30 12:59:30 +1000

    feat: Add check for removed items on migration
    Adds discussion/677177465d72ea02984a6090

--- scripts/migrations.lua
+++ scripts/migrations.lua
@@ -27,6 +27,36 @@ local by_version = {
 --- @param e ConfigurationChangedData
 local function on_configuration_changed(e)
     flib_migration.on_config_changed(e, by_version)
+    for _, player in pairs(game.players) do
+        local self = storage.gui[player.index]
+        if self == nil then
+            return
+        end
+        local tdata = self.tabs["entity"]
+        if tdata == nil then
+            return
+        end
+        for _, preset in pairs(tdata.presets) do
+            local config = preset.config
+            if config == nil then
+                return
+            end
+            for _, c in pairs(config) do
+                if prototypes.entity[c["entity"]] == nil then
+                    c["entity"] = nil
+                end
+                if prototypes.tile[c["tile_0"]] == nil then
+                    c["tile_0"] = nil
+                end
+                if prototypes.tile[c["tile_1"]] == nil then
+                    c["tile_1"] = nil
+                end
+                if prototypes.tile[c["tile_2"]] == nil then
+                    c["tile_2"] = nil
+                end
+            end
+        end
+    end
 end
 
 local migrations = {}
